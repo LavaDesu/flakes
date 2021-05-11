ddfc10bba454054f0ec22812b4bdba84f878a35c
LavaDesu <me@lava.moe>
2021-05-11 21:48:20 +0700

    i forgot to install file managers

--- cfg/winter/packages.nix
+++ cfg/winter/packages.nix
@@ -1,12 +1,13 @@
 { config, pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    git neofetch neovim nodejs rsync wget
+    git lf neofetch neovim nodejs rsync wget
     gnupg
     light glxinfo
-    alacritty discord-canary element-desktop firefox pavucontrol tor-browser-bundle-bin
+    alacritty discord-canary element-desktop firefox gnome3.nautilus pavucontrol tor-browser-bundle-bin
 
     (pkgs.writeShellScriptBin "nix-flakes" ''
       exec ${pkgs.nixUnstable}/bin/nix --experimental-features "nix-command flakes" "$@"
     '')
   ];
+  services.gnome3.sushi.enable = true;
 }
