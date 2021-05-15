4df3f373e98c4ea22f2e698061a0dec7ddd84488
LavaDesu <me@lava.moe>
2021-05-15 08:35:19 +0700

    use grub

--- cfg/winter/kernel.nix
+++ cfg/winter/kernel.nix
@@ -4,6 +4,11 @@
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+      };
     };
     supportedFilesystems = ["bcachefs"];
     blacklistedKernelModules = [
