098e58cb7c09066bd7da5f7654731532b5929076
LavaDesu <me@lava.moe>
2021-05-28 08:27:58 +0700

    bump winetricks

--- overlays/winetricks.nix
+++ overlays/winetricks.nix
@@ -1,12 +1,11 @@
 self: super: {
   winetricks = super.winetricks.overrideAttrs(old: rec {
-    name = "winetricks-20210525-b9030c4";
+    name = "winetricks-20210528-073e2db";
     src = super.fetchFromGitHub {
       repo = "winetricks";
       owner = "Winetricks";
-      rev = "b90e0c4e7cea9acc2c9e89bc4afe873086bbd8a1";
-      sha256 = "11adhgd8zavz4c9yzj0m5570fq7wv6am2wq4j9xkz2655fw2412l";
+      rev = "073e2db522e7db56f83fab2338d831ac716264cb";
+      sha256 = "1fic4wzc3qyw5bki4zx0h9g8yyhh8db806pwm8mz8qv4n7syk4dd";
     };
-    patches = [ ./misc/winetricks.patch ];
   });
 }
