1a2c5f66b859a1648f890a9a87bd3199187a539a
LavaDesu <me@lava.moe>
2021-06-04 13:50:15 +0700

    temporarily use forked home-manager

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    home-manager = { url = "github:nix-community/home-manager"; };
+    home-manager = { url = "github:LavaDesu/home-manager/xsession-profilePath"; };
     secrets = { url = "github:LavaDesu/flakes-secrets"; };
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
