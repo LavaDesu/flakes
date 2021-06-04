34505855136788ba8fc98ecfe641d689fdd15c6a
LavaDesu <me@lava.moe>
2021-06-04 16:19:35 +0700

    bump discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,12 @@
 self: super: {
-  discord-canary = super.discord-canary.override rec {
-    version = "0.0.123";
+  discord-canary = (super.discord-canary.override rec {
+    version = "0.0.124";
     src = builtins.fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "0bijwfsd9s4awqkgxd9c2cxh7y5r06vix98qjp0dkv63r6jig8ch";
+      sha256 = "060ypr9rn5yl8iwh4v3ax1v6501yaq72sx50q47sm0wyxn7gpv91";
     };
-  };
+  }).overrideAttrs(o: {
+    # TODO: delete this when upstream
+    nativeBuildInputs = o.nativeBuildInputs ++ [ super.xorg.libxshmfence ];
+  });
 }
