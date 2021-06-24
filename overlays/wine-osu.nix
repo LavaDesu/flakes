e467153ddcb315d7a902b1bc0d0e79e8e5d7cb01
LavaDesu <me@lava.moe>
2021-06-24 08:44:19 +0700

    pinning wine version

--- overlays/wine-osu.nix
+++ overlays/wine-osu.nix
@@ -1,6 +1,32 @@
-self: super: {
+self: super:
+let
+  wine = super.fetchFromGitHub rec {
+    owner = "wine-mirror";
+    repo = "wine";
+    rev = "1de583a4dac7d704b2d4291ada4a1885cd8cd1c9";
+    sha256 = "02v725qjsibiv6ad6hxsc6199dvlmnp7983286a4k8rygqnvqln9";
+  };
+
+  staging = super.fetchFromGitHub rec {
+    owner = "wine-staging";
+    repo = "wine-staging";
+    rev = "432c0b5a838cb8ebb52d0d37150a57e393b6f33d";
+    sha256 = "0gbci8fjvl1bdz7fj4bh25mqrgi1i04q5na2ckv9hj9nh9x7crbm";
+  };
+in {
   wine-osu = (super.wineStaging.overrideDerivation(o: {
     patches = (o.patches or []) ++ builtins.map (e: ./misc/wine + ("/" + e)) (builtins.attrNames (builtins.readDir ./misc/wine));
+
+    src = wine;
+    postPatch = self.postPatch or "" + ''
+      patchShebangs tools
+      cp -r ${staging}/patches .
+      chmod +w patches
+      cd patches
+      patchShebangs gitapply.sh
+      ./patchinstall.sh DESTDIR="$PWD/.." --all
+      cd ..
+    '';
   })).override {
     wineRelease = "staging";
     wineBuild = "wineWow";
