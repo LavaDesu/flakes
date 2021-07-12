927488c5a90e00d22147929b3eff0734686e009f
LavaDesu <me@lava.moe>
2021-07-12 09:46:15 +0700

    move zsh.sessionVariables to home.sessionVariables

--- users/rin/zsh.nix
+++ users/rin/zsh.nix
@@ -116,31 +116,6 @@ in rec {
       zmodload zsh/complist
     '';
 
-    sessionVariables = {
-      PATH = builtins.concatStringsSep ":" [
-        "${config.home.homeDirectory}/.local/bin"
-        "${config.xdg.dataHome}/npm/bin"
-        "$PATH"
-      ];
-
-      XAUTHORITY = "$XDG_RUNTIME_DIR/Xauthority";
-      EDITOR = "nvim";
-
-      _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.configHome}/java";
-      CARGO_HOME = "${config.xdg.dataHome}/cargo";
-      DIRENV_LOG_FORMAT = "";
-      GNUPGHOME = "${config.xdg.dataHome}/gnupg";
-      GTK2_RC_FILES = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-      LESSHISTFILE = "-";
-      NODE_REPL_HISTORY="${config.xdg.dataHome}/nodejs/repl_history";
-      NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
-      PUB_CACHE = "${config.xdg.cacheHome}/dart";
-      WGETRC = "${config.xdg.configHome}/wgetrc";
-      XINITRC = "${config.xdg.configHome}/xorg/xinitrc";
-
-      WINEPREFIX = "${config.xdg.dataHome}/wine64";
-      WINEARCH = "win64";
-    };
     localVariables = {
       PS1 = "%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b ";
       ZSH_AUTOSUGGEST_STRATEGY = ["completion"];
