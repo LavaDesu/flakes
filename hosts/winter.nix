4408a2124e01f390fd65c53d0db75c95c182bc4f
LavaDesu <me@lava.moe>
2021-07-16 07:23:59 +0700

    move everything else in system config to base module

--- hosts/winter.nix
+++ hosts/winter.nix
@@ -2,16 +2,6 @@
   networking.hostName = "winter";
   system.stateVersion = "20.09";
 
-  environment.etc = {
-    "machine-id".source = "/var/persist/machine-id";
-    "ssh/ssh_host_rsa_key".source = "/var/persist/ssh_host_rsa_key";
-    "ssh/ssh_host_rsa_key.pub".source = "/var/persist/ssh_host_rsa_key.pub";
-    "ssh/ssh_host_ed25519_key".source = "/var/persist/ssh_host_ed25519_key";
-    "ssh/ssh_host_ed25519_key.pub".source = "/var/persist/ssh_host_ed25519_key.pub";
-  };
-  environment.pathsToLink = [ "/share/zsh" ];
-  users.mutableUsers = false;
-
   imports = with modules.system; [
     audio
     base
@@ -28,7 +18,5 @@
 
     ../users/rin.nix
   ];
-
-  i18n.defaultLocale = "en_GB.UTF-8";
 }
 
--- modules/system/base.nix
+++ modules/system/base.nix
@@ -1,4 +1,16 @@
 { config, inputs, modules, overlays, ... }: {
+  environment.etc = {
+    "machine-id".source = "/var/persist/machine-id";
+    "ssh/ssh_host_rsa_key".source = "/var/persist/ssh_host_rsa_key";
+    "ssh/ssh_host_rsa_key.pub".source = "/var/persist/ssh_host_rsa_key.pub";
+    "ssh/ssh_host_ed25519_key".source = "/var/persist/ssh_host_ed25519_key";
+    "ssh/ssh_host_ed25519_key.pub".source = "/var/persist/ssh_host_ed25519_key.pub";
+  };
+  environment.pathsToLink = [ "/share/zsh" ];
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+  users.mutableUsers = false;
+
   system = {
     configurationRevision = inputs.self.rev;
     nixos = rec {
