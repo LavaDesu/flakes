4eef6ff71db4a1a9878855a12781ea5941143025
LavaDesu <me@lava.moe>
2021-09-19 14:16:22 +0700

    discord: bump to 0.0.130

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -1,9 +1,9 @@
 self: super: {
   discord-canary = super.discord-canary.override rec {
-    version = "0.0.129";
+    version = "0.0.130";
     src = builtins.fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "1x49vvd363w9xyrzjgmynj2b320hzpz388fh5vpp0q0qk8q3gwkk";
+      sha256 = "1razxxx16bjzhv9909qa08dmlmgg7ji1vckvkggw7syi125r5aai";
     };
   };
 }
