8a92f00e3b25a2fc7d3e10bfb132aaf8d6fddbaa
LavaDesu <me@lava.moe>
2021-10-06 23:00:21 +0700

    transmission: properly setup rpc

--- modules/system/transmission.nix
+++ modules/system/transmission.nix
@@ -13,6 +13,9 @@
       alt-speed-time-end = 1380;
       alt-speed-up = 256;
       ratio-limit-enabled = true;
+      rpc-bind-address = "0.0.0.0";
+      rpc-enabled = true;
+      rpc-port = 9091;
       rpc-host-whitelist = "${config.networking.hostName}";
       rpc-whitelist = "10.100.0.*,10.0.0.*,192.168.100.*";
     };
