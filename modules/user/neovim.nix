176e0e95131fbbd5b60f67b7f4be0782d51bf4ea
LavaDesu <me@lava.moe>
2021-10-09 20:34:20 +0700

    neovim: move to nvim-lspconfig and lua config

--- modules/user/neovim.nix
+++ modules/user/neovim.nix
@@ -1,21 +1,42 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  nvim-cmp = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "nvim-cmp";
+    version = "2021-10-09";
+    src = pkgs.fetchFromGitHub {
+      owner = "hrsh7th";
+      repo = "nvim-cmp";
+      rev = "339fe9177b8eff85b21a0e118db400ab89775c28";
+      sha256 = "16k9zqqhsw4z4zx1x1y34yss2k2jzq04f4mlp5xc2rwnfxi8yd93";
+    };
+    meta.homepage = "https://github.com/hrsh7th/nvim-cmp/";
+  };
+  luaconf = pkgs.writeText "config.lua"
+    (lib.replaceStrings ["{{OMNISHARP_PATH}}"] ["${pkgs.omnisharp-roslyn}/bin/omnisharp"]
+      (builtins.readFile ../../res/config.lua));
+in {
+  systemd.user.tmpfiles.rules = [
+    "D %t/vim/swap 0755 - - - -"
+    "D %t/vim/undo 0755 - - - -"
+  ];
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
     package = pkgs.neovim-nightly;
+    withNodeJs = true;
+
+    extraPackages = with pkgs; [
+      rust-analyzer
+      nodePackages.diagnostic-languageserver
+      nodePackages.eslint_d
+      nodePackages.typescript-language-server
+      nodePackages.vscode-langservers-extracted
+      nodePackages.yaml-language-server
+    ];
 
     plugins = with pkgs.vimPlugins; [
-      coc-css
-      coc-html
-      coc-nvim
-      coc-eslint
-      coc-json
-      coc-rust-analyzer
-      coc-tsserver
-      coc-yaml
-
       ctrlp-vim
       lualine-nvim
       nerdtree
@@ -25,6 +46,12 @@
       vim-repeat
       vim-signify
       vim-surround
+
+      nvim-cmp
+      nvim-lspconfig
+      cmp-nvim-lsp
+      cmp_luasnip
+      luasnip
 
       (nvim-treesitter.withPlugins (p: with p; [
         tree-sitter-comment
@@ -46,118 +73,9 @@
         tree-sitter-yaml
       ]))
     ];
-    withNodeJs = true;
 
     extraConfig = ''
-      " configuration
-      syntax enable
-      set relativenumber number
-      set cursorline
-      set noswapfile
-      set hlsearch
-      set ignorecase
-      set incsearch
-      set title
-      set clipboard^=unnamed
-      set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
-      set expandtab
-      set updatetime=100
-      let g:yaml_recommended_style=0
-
-      " signify
-      set signcolumn=yes:3
-      let g:signify_priority               = 5
-      let g:signify_sign_show_count        = 0
-      let g:signify_sign_add               = '┃'
-      let g:signify_sign_delete            = g:signify_sign_add
-      let g:signify_sign_delete_first_line = '┏━'
-      let g:signify_sign_change            = g:signify_sign_add
-      let g:signify_sign_change_delete     = g:signify_sign_delete
-
-      " theming
-      let g:tokyonight_style='night'
-      colorscheme tokyonight
-
-      " using tab for trigger completion
-      function! s:check_back_space() abort
-        let col = col('.') - 1
-        return !col || getline('.')[col - 1]  =~ '\s'
-      endfunction
-
-      inoremap <silent><expr> <Tab>
-            \ pumvisible() ? "\<C-n>" :
-            \ <SID>check_back_space() ? "\<Tab>" :
-            \ coc#refresh()
-
-      inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
-      inoremap <silent><expr> <c-space> coc#refresh()
-
-      " various aliases
-      nnoremap <leader>rs :source $MYVIMRC<CR>
-      nnoremap <C-N> :NERDTreeToggle<CR>
-      nnoremap <C-H> <C-W>h
-      nnoremap <C-J> <C-W>j
-      nnoremap <C-K> <C-W>k
-      nnoremap <C-L> <C-W>l
-      nnoremap <C-Q> :q<CR>
-
-      nmap <silent> [g <Plug>(coc-diagnostic-prev)
-      nmap <silent> ]g <Plug>(coc-diagnostic-next)
-      nmap <silent> gd <Plug>(coc-definition)
-      nmap <silent> gy <Plug>(coc-type-definition)
-      nmap <silent> gi <Plug>(coc-implementation)
-      nmap <silent> gr <Plug>(coc-references)
-      nmap <leader>ref <Plug>(coc-references)
-      nmap <leader>gd :Gdiff<CR>
-      nmap <leader>rn <Plug>(coc-rename)
-      command! -nargs=0 Sw w !doas tee % > /dev/null
-
-      " autosave rust source files on change
-      au FileType rust inoremap <silent><esc> <esc>:update<cr>
-      au TextChanged,FocusLost,BufEnter *.rs silent update
-
-      " disable read-only warning
-      au BufEnter * set noro
-
-      " set filetype=html for handlebar templates
-      " au BufRead,BufNewFile *.hbs set filetype=html
-
-      " disable empty line tildes
-      set fcs=eob:\ 
-
-      lua <<EOF
-        require('nvim-treesitter.configs').setup {
-          highlight = { enable = true },
-          indent = { enable = false }
-        }
-        require('lualine').setup {
-          options = {
-            theme = 'tokyonight'
-          }
-        }
-
-        local colors = require("tokyonight.colors").setup({})
-
-        vim.cmd("highlight SignifySignAdd             guifg="..colors.green)
-        vim.cmd("highlight SignifySignChange          guifg="..colors.orange)
-        vim.cmd("highlight SignifySignDelete          guifg="..colors.red)
-        vim.cmd("highlight SignifySignDeleteFirstLine guifg="..colors.red)
-        vim.cmd("highlight SignifySignChangeDelete    guifg="..colors.red)
-      EOF
+      luafile ${luaconf}
     '';
-  };
-  xdg.configFile."nvim/coc-settings.json".text = builtins.toJSON {
-    languageserver = {
-      nix = {
-        command = "rnix-lsp";
-        filetypes = [ "nix" ];
-      };
-    };
-    "eslint.enable" = true;
-    "omnisharp.path" = "${pkgs.omnisharp-roslyn}/bin/omnisharp";
-    "rust-analyzer.lens.enable" = false;
-    "rust-analyzer.inlayHints.enable" = false;
-    "rust-analyzer.serverPath" = pkgs.rust-analyzer + "/bin/rust-analyzer";
-    "typescript.showUnused" = false;
   };
 }
---
+++ res/config.lua
@@ -0,0 +1,236 @@
+-- Keybindings
+local map = vim.api.nvim_set_keymap
+map('n', '<C-H>', '<C-W>h', { noremap = true })
+map('n', '<C-J>', '<C-W>j', { noremap = true })
+map('n', '<C-K>', '<C-W>k', { noremap = true })
+map('n', '<C-L>', '<C-W>l', { noremap = true })
+map('n', '<C-Q>', ':q<CR>', { noremap = true })
+
+-- Autocommands
+vim.cmd('au FileType rust inoremap <silent><esc> <esc>:update<cr>')
+vim.cmd('au TextChanged,FocusLost,BufEnter *.rs silent update')
+vim.cmd('au BufEnter * set noro')
+vim.cmd('au CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({ focusable = false })')
+vim.cmd('au CursorHoldI * silent! lua vim.lsp.buf.signature_help()')
+
+-- Settings
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes:3"
+vim.opt.title = true
+vim.opt.updatetime = 0
+vim.opt.clipboard:prepend('unnamedplus')
+
+local runtimedir = os.getenv('XDG_RUNTIME_DIR')
+vim.opt.directory:prepend(runtimedir..'/vim/swap//')
+vim.opt.undodir:prepend(runtimedir..'/vim/undo//')
+vim.opt.swapfile = true
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+
+vim.g.signify_priority               = 5
+vim.g.signify_sign_show_count        = 0
+vim.g.signify_sign_add               = '┃'
+vim.g.signify_sign_delete            = vim.g.signify_sign_add
+vim.g.signify_sign_delete_first_line = '┏━'
+vim.g.signify_sign_change            = vim.g.signify_sign_add
+vim.g.signify_sign_change_delete     = vim.g.signify_sign_delete
+
+-- Theming
+vim.g.tokyonight_style = 'night'
+vim.cmd[[
+    syntax enable
+    colorscheme tokyonight
+]]
+local colors = require("tokyonight.colors").setup {}
+vim.cmd("highlight SignifySignAdd             guifg="..colors.green)
+vim.cmd("highlight SignifySignChange          guifg="..colors.orange)
+vim.cmd("highlight SignifySignDelete          guifg="..colors.red)
+vim.cmd("highlight SignifySignDeleteFirstLine guifg="..colors.red)
+vim.cmd("highlight SignifySignChangeDelete    guifg="..colors.red)
+
+-- Plugins
+require('nvim-treesitter.configs').setup {
+    highlight = { enable = true },
+    indent = { enable = false }
+}
+require('lualine').setup {
+    options = {
+        theme = 'tokyonight'
+    }
+}
+
+-- Block comments indent workaround
+-- many thanks to @kristijanhusak
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/1167#issuecomment-920824125
+function _G.javascript_indent()
+  local line = vim.fn.getline(vim.v.lnum)
+  local prev_line = vim.fn.getline(vim.v.lnum - 1)
+  if line:match('^%s*[%*/]%s*') then
+    if prev_line:match('^%s*%*%s*') then
+      return vim.fn.indent(vim.v.lnum - 1)
+    end
+    if prev_line:match('^%s*/%*%*%s*$') then
+      return vim.fn.indent(vim.v.lnum - 1) + 1
+    end
+  end
+
+  return vim.fn['GetJavascriptIndent']()
+end
+
+vim.cmd('au FileType javascript setlocal indentexpr=v:lua.javascript_indent()')
+
+-- LSP
+local nvim_lsp = require('lspconfig')
+
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local opts = { noremap = true, silent = true }
+
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
+        underline = true,
+        signs = true,
+        update_in_insert = true
+    }
+)
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = { 'cssls', 'html', 'rnix', 'rust_analyzer', 'tsserver', 'yamlls' }
+for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        flags = {
+            debounce_text_changes = 150,
+        }
+    }
+end
+
+-- nvim-cmp
+local luasnip = require('luasnip')
+local cmp = require('cmp')
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-N>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+            else
+                fallback()
+            end
+        end
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }
+    }
+}
+
+-- LSP/Omnisharp
+local pid = vim.fn.getpid()
+require'lspconfig'.omnisharp.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = { "{{OMNISHARP_PATH}}", "--languageserver", "--hostPID", tostring(pid) }
+}
+
+-- LSP/Diagnostics
+nvim_lsp.diagnosticls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    init_options = {
+        linters = {
+            eslint = {
+                command = 'eslint',
+                rootPatterns = { '.git' },
+                debounce = 100,
+                args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+                sourceName = 'eslint',
+                parseJson = {
+                    errorsRoot = '[0].messages',
+                    line = 'line',
+                    column = 'column',
+                    endLine = 'endLine',
+                    endColumn = 'endColumn',
+                    message = '[eslint] ${message} [${ruleId}]',
+                    security = 'severity'
+                },
+                securities = {
+                    [2] = 'error',
+                    [1] = 'warning'
+                }
+            }
+        },
+        filetypes = {
+            javascript = 'eslint',
+            javascriptreact = 'eslint',
+            typescript = 'eslint',
+            typescriptreact = 'eslint'
+        }
+    }
+}
