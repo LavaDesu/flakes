3a5115c86a4b1ea4a45df1180a5d36ed58689628
LavaDesu <me@lava.moe>
2021-11-28 20:36:42 +0700

    user/direnv: clear cache on activation
    also changed the layout name to be a bit easier to find if need be

--- modules/user/direnv.nix
+++ modules/user/direnv.nix
@@ -1,4 +1,4 @@
-{ config, ...}: {
+{ config, lib, ... }: {
   programs.direnv = {
     enable = true;
     nix-direnv = {
@@ -8,4 +8,9 @@
   programs.git.extraConfig.core.excludesFile = ".envrc";
   # We can't use .source since hm manages this file too
   xdg.configFile."direnv/direnvrc".text = builtins.readFile ../../res/direnvrc;
+  home.activation = {
+    direnvClearCache = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      $DRY_RUN_CMD rm -rf $VERBOSE_ARG $HOME/.cache/direnv/layouts
+    '';
+  };
 }
