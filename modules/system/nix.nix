7b8d93bd7ae0da41149341a32a2225c496bc7a30
LavaDesu <me@lava.moe>
2021-12-07 13:38:10 +0700

    system/nix: use nix 2.5pre20211007_844dd90

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,6 +1,16 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   nix = rec {
-    package = pkgs.nixUnstable;
+    # XXX: remove after upstream fixes https://github.com/NixOS/nix/issues/5728
+    package = pkgs.nixUnstable.overrideAttrs(_: rec {
+      version = "2.5${suffix}";
+      suffix = "pre20211007_${lib.substring 0 7 src.rev}";
+      src = pkgs.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nix";
+        rev = "844dd901a7debe8b03ec93a7f717b6c4038dc572";
+        sha256 = "sha256-fe1B4lXkS6/UfpO0rJHwLC06zhOPrdSh4s9PmQ1JgPo=";
+      };
+    });
     binaryCaches = [
       "https://cache.nixos.org?priority=10"
       "https://lava.cachix.org"
