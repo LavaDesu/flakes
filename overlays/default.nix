ff5273b7208d49704247a3f748c7ba221c7fb083
LavaDesu <me@lava.moe>
2021-12-18 19:44:10 +0700

    overlays/nix: init from system/nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 builtins.map (path: import path) [
   ./discord.nix
   ./material-icons.nix
+  ./nix.nix
   ./picom.nix
   ./rofi.nix
   ./xinit.nix
---
+++ overlays/nix.nix
@@ -0,0 +1,13 @@
+# TODO: remove after upstream fixes https://github.com/NixOS/nix/issues/5728
+self: super: {
+  nixUnstable = super.nixUnstable.overrideAttrs(_: rec {
+    version = "2.5${suffix}";
+    suffix = "pre20211007_${self.lib.substring 0 7 src.rev}";
+    src = self.fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nix";
+      rev = "844dd901a7debe8b03ec93a7f717b6c4038dc572";
+      sha256 = "sha256-fe1B4lXkS6/UfpO0rJHwLC06zhOPrdSh4s9PmQ1JgPo=";
+    };
+  });
+}
