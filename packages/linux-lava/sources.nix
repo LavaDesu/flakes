f768eb6fa770359e02568c39a7fdcc0c7b8ec07b
LavaDesu <me@lava.moe>
2022-02-02 17:18:47 +0700

    packages/linux-lava: bump to 5.17.0-rc2

--- packages/linux-lava/sources.nix
+++ packages/linux-lava/sources.nix
@@ -1,51 +1,48 @@
 { fetchFromGitHub, inputs, lib }:
 let
-  version = "5.15.13";
-  kernelHash = "1s0yk78kilcr3vd14k67cmllnrx0x0i00jdkl5kkn3lij5lwzcjp";
-  kernelPatchHash = "044y7mmla0f73mky24vpvl8ba3warfr6im97s1231gjxican40v6";
+  rc = "-rc2";
+  version = "5.17.0${rc}";
+  kernelHash = "14za6421w889a7j8skd63r0gywnha0kjbl5m2wwi4j8y6d6jim4d";
+  #kernelPatchHash = "044y7mmla0f73mky24vpvl8ba3warfr6im97s1231gjxican40v6";
 
   tkgPatches = [
     "0001-mm-Support-soft-dirty-flag-reset-for-VA-range"
     "0002-clear-patches"
     "0002-mm-Support-soft-dirty-flag-read-with-reset"
     "0003-glitched-base"
-    "0007-v${mm}-futex_waitv"
     "0007-v${mm}-fsync1_via_futex_waitv"
     "0007-v${mm}-winesync"
-    "0009-prjc_v5.15-r1"
+    "0009-prjc_v5.17-r0"
     "0009-glitched-ondemand-bmq"
     "0005-glitched-pds"
     "0012-misc-additions"
   ];
 
-
   patch = path: {
     name = "patch-${path}";
     patch = path;
   };
-  kernelPatchSrc = {
-    name = "patch";
-    patch = builtins.fetchurl {
-      url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/patch-${version}.xz";
-      sha256 = kernelPatchHash;
-    };
-  };
+  # kernelPatchSrc = {
+  #   name = "patch";
+  #   patch = builtins.fetchurl {
+  #     url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/patch-${version}.xz";
+  #     sha256 = kernelPatchHash;
+  #   };
+  # };
   mm = lib.versions.majorMinor version;
 in {
   inherit version;
 
   src = builtins.fetchurl {
-    url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/linux-${mm}.tar.xz";
+    url = "https://git.kernel.org/torvalds/t/linux-${mm}${rc}.tar.gz";
     sha256 = kernelHash;
   };
+  # src = builtins.fetchurl {
+  #   url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/linux-${mm}.tar.xz";
+  #   sha256 = kernelHash;
+  # };
 
   kernelPatches = [
-    # This is dumb but nix strips FHS paths *before* patching (why???????) so patches might fail to apply
-    # This "unpatch patch" fixes one such patch failure, then reapplys the stripped FHS path in
-    # the "repatch patch" (even though the affected file isn't really used but w/e)
-    (patch ./unpatch.patch)
-    kernelPatchSrc
-    (patch ./repatch.patch)
     (patch ./si-manual-clocking.patch)
   ]
   ++ builtins.map (name: {
