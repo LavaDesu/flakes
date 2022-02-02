406df074d54deec26665326bfc32989150a5b7f4
LavaDesu <me@lava.moe>
2022-02-02 16:11:09 +0700

    sugarcane/filesystem: fixup filesystems

--- hosts/sugarcane/filesystem.nix
+++ hosts/sugarcane/filesystem.nix
@@ -12,22 +12,14 @@ in {
     "/" = {
       device = "rootfs";
       fsType = "tmpfs";
-      options = [ "defaults" "size=2G" "mode=755" ];
+      options = [ "defaults" "size=1G" "mode=755" ];
     };
 
-    "/mnt" = {
+    "/nix" = {
       device = "/dev/disk/by-uuid/19d572a8-1cf6-4b9c-94c6-3ce6be54f719";
       fsType = "ext4";
       options = [ "defaults" "noatime" ];
       neededForBoot = true;
-    };
-
-    "/nix" = {
-      depends = [ "/mnt" ];
-      device = "/mnt/nix";
-      fsType = "none";
-      neededForBoot = true;
-      options = [ "bind" ];
     };
 
     "/var/persist" = bind "/nix/persist";
