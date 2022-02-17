3f26f7186771b871eaff4c5b11b5419f5a99377e
LavaDesu <me@lava.moe>
2022-02-17 09:54:46 +0700

    services/nginx: return 444 on unknown domains

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -14,6 +14,16 @@
         forceSSL = true;
         root = inputs.website.outPath;
       };
+      "_" = {
+        default = true;
+        addSSL = true;
+        # TODO generate this somewhere
+        sslCertificate = "/persist/fakeCerts/fake.crt";
+        sslCertificateKey = "/persist/fakeCerts/fake.key";
+        extraConfig = ''
+          return 444;
+        '';
+      };
     };
   };
 }
