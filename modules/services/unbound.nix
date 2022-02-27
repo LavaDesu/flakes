b0923384df40a2affbab47ab515e15b984ea1e2a
LavaDesu <me@lava.moe>
2022-02-27 12:00:51 +0700

    services/unbound: allow 10.0.0.0/8

--- modules/services/unbound.nix
+++ modules/services/unbound.nix
@@ -25,6 +25,7 @@ in {
         interface = [ "0.0.0.0" ];
         access-control = [
           "127.0.0.1/8      allow"
+          "10.0.0.0/8       allow"
           "192.168.100.0/24 allow"
         ];
       };
