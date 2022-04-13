e3f65a429a938893ebd337a6ad6cfb963e5d32ac
LavaDesu <me@lava.moe>
2022-04-13 22:06:22 +0700

    packages/linux-lava: bump to 5.17.2

--- packages/linux-lava/sources.nix
+++ packages/linux-lava/sources.nix
@@ -1,9 +1,8 @@
 { fetchFromGitHub, inputs, lib }:
 let
-  rc = "-rc5";
-  version = "5.17.0${rc}";
-  kernelHash = "0cy1m2ylvay4ai2ivy92cpxvck8ra4i99i65zsvfyjp3gbvy13gm";
-  #kernelPatchHash = "044y7mmla0f73mky24vpvl8ba3warfr6im97s1231gjxican40v6";
+  version = "5.17.2";
+  kernelHash = "1cdi43x4c3l4chznh57gm55szycj4wjlxl1dss1ilnfvvmhyypsm";
+  kernelPatchHash = "0rsril9dpvxy4afc6agx8gsyhmbicww43dzmjhmxa8c4h6wdj8bx";
 
   tkgPatches = [
     "0001-mm-Support-soft-dirty-flag-reset-for-VA-range"
@@ -22,27 +21,28 @@ let
     name = "patch-${path}";
     patch = path;
   };
-  # kernelPatchSrc = {
-  #   name = "patch";
-  #   patch = builtins.fetchurl {
-  #     url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/patch-${version}.xz";
-  #     sha256 = kernelPatchHash;
-  #   };
-  # };
+  kernelPatchSrc = {
+    name = "patch";
+    patch = builtins.fetchurl {
+      url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/patch-${version}.xz";
+      sha256 = kernelPatchHash;
+    };
+  };
   mm = lib.versions.majorMinor version;
 in {
   inherit version;
 
-  src = builtins.fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-${mm}${rc}.tar.gz";
-    sha256 = kernelHash;
-  };
   # src = builtins.fetchurl {
-  #   url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/linux-${mm}.tar.xz";
+  #   url = "https://git.kernel.org/torvalds/t/linux-${mm}${rc}.tar.gz";
   #   sha256 = kernelHash;
   # };
+  src = builtins.fetchurl {
+    url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/linux-${mm}.tar.xz";
+    sha256 = kernelHash;
+  };
 
   kernelPatches = [
+    kernelPatchSrc
     (patch ./si-manual-clocking.patch)
   ]
   ++ builtins.map (name: {
