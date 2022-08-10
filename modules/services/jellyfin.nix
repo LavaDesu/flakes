13d0e1ddbda46edca78b5c5fe2d51fcc86a7b863
LavaDesu <me@lava.moe>
2022-08-10 15:50:45 +0700

    services/jellyfin: use bind mounts

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -3,9 +3,23 @@ let
   dir = "/persist/jellyfin";
 in
 {
+  fileSystems."/var/lib/jellyfin" = {
+    depends = [ "/persist" ];
+    device = dir;
+    fsType = "none";
+    options = [ "bind" ];
+  };
+  system.activationScripts."jellyfin-create-bind-mount" = {
+    deps = [ "users" "groups" ];
+    text = ''
+      mkdir ${dir}
+      chown jellyfin:jellyfin ${dir}
+      chmod 700 ${dir}
+    '';
+  };
   systemd.tmpfiles.rules = [
-    "d ${dir} 700 jellyfin jellyfin"
-    "L /var/lib/jellyfin - - - - ${dir}"
+    "d /tmp/jelly-transcodes 700 jellyfin jellyfin"
+    "L+ /var/lib/jellyfin/transcodes - - - - /tmp/jelly-transcodes"
   ];
   services.jellyfin = {
     enable = true;
