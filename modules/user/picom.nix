189c27b289ceb1ebe27682e47a7e70b73cbd5500
LavaDesu <me@lava.moe>
2022-08-26 19:53:52 +0700

    user/picom: fixes

--- modules/user/picom.nix
+++ modules/user/picom.nix
@@ -3,35 +3,32 @@
     enable = true;
     experimentalBackends = true;
 
-    blur = true;
-    blurExclude = [
-      (builtins.concatStringsSep " && " [
-        "class_g != 'Alacritty'"
-        "class_g != 'kitty'"
-        #"class_g != 'Polybar'"
-        "class_g != 'URxvt'"
-      ])
-    ];
+    # blur = true;
+    # blurExclude = [
+    #   (builtins.concatStringsSep " && " [
+    #     "class_g != 'Alacritty'"
+    #     "class_g != 'kitty'"
+    #     #"class_g != 'Polybar'"
+    #     "class_g != 'URxvt'"
+    #   ])
+    # ];
     fade = true;
     fadeDelta = 5;
-    fadeSteps = [ "0.05" "0.05" ];
+    fadeSteps = [ 0.05 0.05 ];
 
-    opacityRule = [
+    opacityRules = [
       "80:class_g = 'Alacritty' && !focused"
       "95:class_g = 'kitty' && !focused"
     ];
     vSync = true;
 
-    extraOptions = ''
+    settings = {
       glx-no-stencil = true;
       glx-copy-from-front = false;
       glx-no-rebind-pixmap = true;
       # https://github.com/yshui/picom/issues/578
       glx-use-copysubbuffer-mesa = false;
       use-damage = true;
-
-      blur-method = "dual_kawase";
-      blur-strength = 15;
 
       detect-transient = true;
       detect-client-leader = true;
@@ -40,6 +37,6 @@
       use-ewmh-active-win = true;
       mark-wmwin-focused = true;
       mark-ovredir-focused = true;
-    '';
+    };
   };
 }
