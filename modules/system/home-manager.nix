f5d2b228948c83b7ee55299752583a58f6bdff39
LavaDesu <me@lava.moe>
2022-10-03 21:15:24 +0700

    system/home-manager: revert git workaround

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -6,13 +6,5 @@
       inherit enableGUI inputs modules;
       sysConfig = config;
     };
-    # uncomment to prevent nmd IFD
-    # sharedModules = [ { manual.manpages.enable = false; } ];
-
-    # HACK: due to git cve, nixos-rebuild --flake breaks; revert after nix is fixed
-    users.root.programs.git = {
-      enable = true;
-      extraConfig.safe.directory = "*";
-    };
   };
 }
