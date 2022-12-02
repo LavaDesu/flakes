771f1a466fe94ee636c635a07931537d8369d38a
LavaDesu <me@lava.moe>
2022-12-02 23:25:54 +0700

    user/picom: remove experimentalBackends

--- modules/user/picom.nix
+++ modules/user/picom.nix
@@ -1,17 +1,7 @@
 { config, ... }: {
   services.picom = {
     enable = true;
-    experimentalBackends = true;
 
-    # blur = true;
-    # blurExclude = [
-    #   (builtins.concatStringsSep " && " [
-    #     "class_g != 'Alacritty'"
-    #     "class_g != 'kitty'"
-    #     #"class_g != 'Polybar'"
-    #     "class_g != 'URxvt'"
-    #   ])
-    # ];
     fade = true;
     fadeDelta = 5;
     fadeSteps = [ 0.05 0.05 ];
