506cb016ad1de1bd94e6bae531fef4b1fd8b4d4a
LavaDesu <me@lava.moe>
2022-12-02 21:33:17 +0700

    system/nix-stable: fix rename warning

--- modules/system/nix-stable.nix
+++ modules/system/nix-stable.nix
@@ -1,14 +1,16 @@
 { config, lib, pkgs, ... }: {
-  nix = rec {
-    binaryCaches = [
-      "https://cache.nixos.org?priority=10"
-      "https://lava.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "lava.cachix.org-1:8lTWI/3IKWHByzzYHZySunMPYs2eAJw2duL+uLZkSy0="
-    ];
-    trustedBinaryCaches = binaryCaches;
+  nix = {
+    settings = rec {
+      substituters = [
+        "https://cache.nixos.org?priority=10"
+        "https://lava.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "lava.cachix.org-1:8lTWI/3IKWHByzzYHZySunMPYs2eAJw2duL+uLZkSy0="
+      ];
+      trusted-substituters = substituters;
+    };
 
     extraOptions = ''
       experimental-features = nix-command flakes
