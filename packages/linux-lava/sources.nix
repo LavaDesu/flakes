88d22748085db952c90dbf6e6183547e6c9d0c53
LavaDesu <me@lava.moe>
2022-12-28 20:11:51 +0700

    packages/linux-lava: bump to 6.1.1 and remove atomic pageflip patch

--- packages/linux-lava/sources.nix
+++ packages/linux-lava/sources.nix
@@ -1,8 +1,8 @@
 { fetchFromGitHub, inputs, lib }:
 let
-  version = "6.0.0";
-  kernelHash = "13kqh7yhifwz5dmd3ky0b3mzbh9r0nmjfp5mxy42drcdafjl692w";
-  #kernelPatchHash = "0y25zs7rlk9dz9ppyr76rv6id64j3f4hpby4wljmy7p9z9wba30s";
+  version = "6.1.1";
+  kernelHash = "1ssxn81jfl0jf9brczfrrwd1f1vmf594jvhrs7zgcc54a5qg389c";
+  kernelPatchHash = "0qmmg4lmz8qhbdc2k6lhavbfak2r64jr8ac16wvnm1x72jwcsil9";
 
   mm = lib.versions.majorMinor version;
   tkgPatches = [
@@ -14,7 +14,6 @@ let
     "0003-glitched-cfs-additions"
     "0007-v${mm}-fsync1_via_futex_waitv"
     "0007-v${mm}-winesync"
-    "0010-lru_${mm}"
     "0012-misc-additions"
   ];
 
@@ -22,19 +21,19 @@ let
     name = "patch-${path}";
     patch = path;
   };
-  # kernelPatchSrc = {
-  #   name = "patch";
-  #   patch = builtins.fetchurl {
-  #     url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/patch-${version}.xz";
-  #     sha256 = kernelPatchHash;
-  #   };
-  # };
+  kernelPatchSrc = {
+    name = "patch";
+    patch = builtins.fetchurl {
+      url = "https://cdn.kernel.org/pub/linux/kernel/v${lib.versions.major version}.x/patch-${version}.xz";
+      sha256 = kernelPatchHash;
+    };
+  };
 
   borePatch = {
     name = "bore-patch";
     patch = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/faaa50992e7ddea9191eb51920ffbf41226c9741/${mm}/sched/0001-bore.patch";
-      sha256 = "1a3vypmrlr20fdvzshgqad3c64bh5hd4295p42j0260dz7vzbf7v";
+      url = "https://raw.githubusercontent.com/CachyOS/kernel-patches/78e1cd84c173959e87cca10f648fbcfbb37fbad2/${mm}/sched/0001-bore.patch";
+      sha256 = "10dfd15zhvkvlk29zqlmgxm99y3lbkjlilvrbrdl8rjcybs4gbws";
     };
   };
 in {
@@ -50,11 +49,11 @@ in {
   };
 
   kernelPatches = [
-    #kernelPatchSrc
+    kernelPatchSrc
     borePatch
     (patch ./si-manual-clocking.patch)
-    (patch ./atomic-async-page-flips.patch)
-    (patch ./winesync-hotfix.patch)
+    #(patch ./atomic-async-page-flips.patch)
+    #(patch ./winesync-hotfix.patch)
   ]
   ++ builtins.map (name: {
     inherit name;
