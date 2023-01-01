0c0ebdcd1c8e162b149afbe30170a55ee4055f05
LavaDesu <me@lava.moe>
2023-01-01 22:34:55 +0700

    user/neovim: fix deprecation

--- res/config.lua
+++ res/config.lua
@@ -139,7 +139,7 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local servers = { 'cssls', 'html', 'rnix', 'tsserver', 'yamlls' }
 for _, lsp in ipairs(servers) do
