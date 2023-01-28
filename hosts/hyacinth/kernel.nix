22187adb1ba8bd37cb13c7bcb9379cc947188cc1
LavaDesu <me@lava.moe>
2023-01-29 00:01:08 +0700

    hyacinth/kernel: disable split_lock detection

--- hosts/hyacinth/kernel.nix
+++ hosts/hyacinth/kernel.nix
@@ -9,6 +9,7 @@
     kernelParams = [
       "amdgpu.gpu_recovery=1"
       "intel_pstate=passive"
+      "split_lock_detect=off"
     ];
     kernelPackages = lib.mkForce (pkgs.linuxPackagesFor pkgs.me.linux-lava);
 
