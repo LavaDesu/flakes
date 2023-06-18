1eb9d03303bd42ffc4df5c8fc16b090fa1ceed0c
LavaDesu <me@lava.moe>
2023-06-18 21:29:25 +0700

    packages/tree-sitter-*: rename source to src

--- packages/tree-sitter-jsonc/default.nix
+++ packages/tree-sitter-jsonc/default.nix
@@ -5,5 +5,5 @@
 callPackage (inputs.nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix") {} {
   language = "jsonc";
   version = "1.0.0";
-  source = inputs.tree-sitter-jsonc;
+  src = inputs.tree-sitter-jsonc;
 }
