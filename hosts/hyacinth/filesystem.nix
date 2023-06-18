859dfb95525cf87239d20b0a3f456e4a3188bb28
LavaDesu <me@lava.moe>
2023-06-18 21:19:05 +0700

    system/snapper: move to new config

--- hosts/hyacinth/filesystem.nix
+++ hosts/hyacinth/filesystem.nix
@@ -36,17 +36,15 @@ in
     };
   };
   services.snapper.configs.cream = {
-      fstype = "btrfs";
-      subvolume = "/mnt/cream/permanence";
-      extraConfig = lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "${k}=${v}") {
-        TIMELINE_CLEANUP = "yes";
-        TIMELINE_CREATE = "yes";
-        TIMELINE_MIN_AGE = "1800";
-        TIMELINE_LIMIT_HOURLY = "5";
-        TIMELINE_LIMIT_DAILY = "7";
-        TIMELINE_LIMIT_WEEKLY = "0";
-        TIMELINE_LIMIT_MONTHLY = "0";
-        TIMELINE_LIMIT_YEARLY = "0";
-      });
+      FSTYPE = "btrfs";
+      SUBVOLUME = "/mnt/cream/permanence";
+      TIMELINE_CLEANUP = "yes";
+      TIMELINE_CREATE = "yes";
+      TIMELINE_MIN_AGE = "1800";
+      TIMELINE_LIMIT_HOURLY = "5";
+      TIMELINE_LIMIT_DAILY = "7";
+      TIMELINE_LIMIT_WEEKLY = "0";
+      TIMELINE_LIMIT_MONTHLY = "0";
+      TIMELINE_LIMIT_YEARLY = "0";
     };
 }
