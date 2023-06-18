a422ee60473a24db3d47867e3af34867c50e4fd8
LavaDesu <me@lava.moe>
2023-06-18 21:19:05 +0700

    system/snapper: move to new config

--- modules/system/snapper.nix
+++ modules/system/snapper.nix
@@ -2,18 +2,16 @@
   services.snapper = {
     cleanupInterval = "1h";
     configs.home = {
-      fstype = "btrfs";
-      subvolume = "/home";
-      extraConfig = lib.concatStringsSep "\n" (lib.mapAttrsToList (k: v: "${k}=${v}") {
-        TIMELINE_CLEANUP = "yes";
-        TIMELINE_CREATE = "yes";
-        TIMELINE_MIN_AGE = "1800";
-        TIMELINE_LIMIT_HOURLY = "5";
-        TIMELINE_LIMIT_DAILY = "7";
-        TIMELINE_LIMIT_WEEKLY = "0";
-        TIMELINE_LIMIT_MONTHLY = "0";
-        TIMELINE_LIMIT_YEARLY = "0";
-      });
+      FSTYPE = "btrfs";
+      SUBVOLUME = "/home";
+      TIMELINE_CLEANUP = "yes";
+      TIMELINE_CREATE = "yes";
+      TIMELINE_MIN_AGE = "1800";
+      TIMELINE_LIMIT_HOURLY = "5";
+      TIMELINE_LIMIT_DAILY = "7";
+      TIMELINE_LIMIT_WEEKLY = "0";
+      TIMELINE_LIMIT_MONTHLY = "0";
+      TIMELINE_LIMIT_YEARLY = "0";
     };
   };
 }
