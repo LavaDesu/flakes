1747b9c918b2d8a3870c9fef9a4fe61bb089669f
LavaDesu <me@lava.moe>
2023-08-18 21:14:58 +0700

    services/postgres: enable tcp/ip

--- modules/services/postgres.nix
+++ modules/services/postgres.nix
@@ -9,8 +9,10 @@ in {
     enable = true;
     dataDir = dir;
     authentication = lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      #type  database  DBuser  origin-address      auth-method
+      local  all       all                         trust
+      host   all       all     127.0.0.1/32        trust
+      host   all       all     ::1/128             trust
     '';
   };
 }
