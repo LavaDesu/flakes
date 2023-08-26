8993e19d3389e32404287b4dea2c66e039b7f5a7
LavaDesu <me@lava.moe>
2023-08-26 14:27:12 +0700

    overlays/eww: remove patch

--- overlays/eww.nix
+++ overlays/eww.nix
@@ -3,8 +3,6 @@ self: super: {
     patches = old.patches ++ [
       # Use normal scroll events instead of smooth scroll ( due to https://bugzilla.gnome.org/show_bug.cgi?id=675959 )
       ./patches/eww.patch
-      # Backport https://github.com/elkowar/eww/pull/711
-      ./patches/eww-box.patch
     ];
   });
 }
