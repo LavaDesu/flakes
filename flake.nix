dff42e604b065b721746fe298bfe56b59381d5d2
LavaDesu <me@lava.moe>
2023-12-15 23:10:13 +0700

    flake: track nixos-unstable instead of master

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-raccoon.url = "github:NixOS/nixpkgs/nixos-22.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager-raccoon.url = "github:nix-community/home-manager/release-22.11";
