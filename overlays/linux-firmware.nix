8fbe93c70300c863b9d9d09d6a173a783e98ac34
LavaDesu <me@lava.moe>
2024-01-16 15:49:15 +0700

    lifirm

--- overlays/linux-firmware.nix
+++ overlays/linux-firmware.nix
@@ -1,7 +1,7 @@
 self: super: {
   linux-firmware = super.linux-firmware.overrideAttrs(o: {
-    postInstall = ''
-    ls -al
+    installPhase = ''
+      ls -al
       cd cirrus
       ln -s cs35l41-dsp1-spk-cali-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-cali-10431683-spkid0-l0.bin.xz
       ln -s cs35l41-dsp1-spk-cali-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-cali-10431683-spkid0-r0.bin.xz
