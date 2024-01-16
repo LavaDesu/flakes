d0e6b9b715a14afb161d7abbaca841d842c66b06
LavaDesu <me@lava.moe>
2024-01-16 16:05:32 +0700

    ftest

--- hosts/anemone/default.nix
+++ hosts/anemone/default.nix
@@ -9,6 +9,7 @@
     #wg_hyacinth.file = ../../secrets/wg_blossom.age;
     #wpa_conf.file = ../../secrets/wpa_conf.age;
   };
+
   imports = with modules.system; [
     inputs.home-manager.nixosModule
     home-manager
@@ -39,6 +40,27 @@
   environment.systemPackages = with pkgs; [ wpa_supplicant_gui ];
 
   programs.hyprland.enable = true;
+
+  hardware.firmware = [
+      (
+    pkgs.runCommandNoCC "cs35l41-10431683" { } ''
+      mkdir -p $out/lib/firmware/cirrus
+      cd $out/lib/firmware/cirrus
+      ln -s ./cs35l41-dsp1-spk-cali-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-cali-10431683-spkid0-l0.bin.xz
+      ln -s ./cs35l41-dsp1-spk-cali-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-cali-10431683-spkid0-r0.bin.xz
+      ln -s ./cs35l41-dsp1-spk-cali-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-cali-10431683-spkid1-l0.bin.xz
+      ln -s ./cs35l41-dsp1-spk-cali-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-cali-10431683-spkid1-r0.bin.xz
+
+      ln -s ./cs35l41-dsp1-spk-prot-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-prot-10431683-spkid0-l0.bin.xz
+      ln -s ./cs35l41-dsp1-spk-prot-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-prot-10431683-spkid0-r0.bin.xz
+      ln -s ./cs35l41-dsp1-spk-prot-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-prot-10431683-spkid1-l0.bin.xz
+      ln -s ./cs35l41-dsp1-spk-prot-10431e12-spkid0-l0.bin.xz cs35l41-dsp1-spk-prot-10431683-spkid1-r0.bin.xz
+
+      ln -s ./cs35l41-dsp1-spk-cali-10431e12.wmfw.xz cs35l41-dsp1-spk-cali-10431683.wmfw.xz
+      ln -s ./cs35l41-dsp1-spk-prot-10431e12.wmfw.xz cs35l41-dsp1-spk-prot-10431683.wmfw.xz
+    ''
+  )
+  ];
 
   # For steam fhs-env
   nixpkgs.config.permittedInsecurePackages = [
