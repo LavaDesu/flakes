fb9bedc9409cfe94d87130e1e9aa069861991e6b
LavaDesu <me@lava.moe>
2024-03-14 13:55:31 +1100

    anemone/networking: use wg-quick

--- hosts/anemone/networking.nix
+++ hosts/anemone/networking.nix
@@ -3,6 +3,8 @@
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
     wireless.enable = true;
 
+    wg-quick.interfaces.wg0.configFile = "/persist/vpn.conf";
+
     networkmanager = {
       enable = true;
       dns = "none";
