d9e8c2bd34309b9f1a5f2607f7d5d5f488a0a976
LavaDesu <me@lava.moe>
2024-07-07 12:12:32 +1000

    user/gpg: fix pinentry

--- modules/user/gpg.nix
+++ modules/user/gpg.nix
@@ -1,10 +1,10 @@
-{ config, enableGUI, ... }: {
+{ config, pkgs, ... }: {
   programs.gpg = {
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
   };
   services.gpg-agent = {
     enable = true;
-    pinentryFlavor = if enableGUI then "gnome3" else "tty";
+    pinentryPackage = pkgs.pinentry-gnome3;
   };
 }
