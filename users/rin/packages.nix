a669c4519b9fc6c5d578207d870a66770a6f4d3d
LavaDesu <me@lava.moe>
2024-10-23 14:02:51 +1100

    packages/psensor: bring back

--- users/rin/packages.nix
+++ users/rin/packages.nix
@@ -39,11 +39,11 @@
     maim
     mangohud
     #me.tetrio-desktop-plus
+    me.psensor
     inputs.nix-gaming.packages.x86_64-linux.osu-lazer-bin
     #inputs.nix-gaming.packages.x86_64-linux.wine-osu
     pavucontrol
     prismlauncher
-    psensor
     qbittorrent
     rivalcfg
     screenkey
