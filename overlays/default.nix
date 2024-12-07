0785ab9bcd2607199ba4f3e20efaa92183b88975
LavaDesu <me@lava.moe>
2024-12-07 21:09:59 +1100

    overlays/{discord,picom}: remove

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,10 +1,8 @@
 builtins.map (path: import path) [
   ./android-studio.nix
   ./ccache.nix
-  ./discord.nix
   ./eww.nix
   ./material-icons.nix
-  ./picom.nix
   ./rofi.nix
   ./steam.nix
   ./utillinux.nix
--- overlays/discord.nix
+++
@@ -1,9 +0,0 @@
-self: super: {
-  discord-canary = super.discord-canary.override rec {
-    version = "0.0.166";
-    src = builtins.fetchurl {
-      url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "02sa34jjwwpf82w492qpk9k2jjfg2lkh1f2z4w5l0zyll6jwjikd";
-    };
-  };
-}
