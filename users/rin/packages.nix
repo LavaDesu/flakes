2e2131257084efd920cf27147ec7c445d68c9f0a
LavaDesu <me@lava.moe>
2024-12-19 23:04:56 +1100

    rin/packages: add osu-lazer and osu-wine

--- users/rin/packages.nix
+++ users/rin/packages.nix
@@ -39,8 +39,8 @@
     mangohud
     #me.tetrio-desktop-plus
     me.psensor
-    #inputs.nix-gaming.packages.x86_64-linux.osu-lazer-bin
-    #inputs.nix-gaming.packages.x86_64-linux.wine-osu
+    inputs.nix-gaming.packages.x86_64-linux.osu-lazer-bin
+    inputs.nix-gaming.packages.x86_64-linux.wine-osu
     pavucontrol
     prismlauncher
     qbittorrent
