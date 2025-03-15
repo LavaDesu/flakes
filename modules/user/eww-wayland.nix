88196824f658eaf422a3ba7fc3a1bdec93082d1a
LavaDesu <me@lava.moe>
2025-03-15 20:20:13 +1100

    user/eww-wayland: use catppuccin colours

--- modules/user/eww-wayland.nix
+++ modules/user/eww-wayland.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  res = pkgs.stdenvNoCC.mkDerivation {
+    pname = "eww-wayland-config";
+    version = "1.0.0";
+    dontUnpack = true;
+    installPhase = ''
+      cp -r ${../../res/eww-wayland} $out
+      substituteInPlace $out/eww.scss \
+        --replace-warn "EWW_BACKGROUND" "${config.catppuccin.hexcolors.surface0}" \
+        --replace-warn "EWW_TEXT" "${config.catppuccin.hexcolors.text}" \
+        --replace-warn "EWW_ACCENT" "${config.catppuccin.hexcolors.pink}"
+    '';
+  };
+in {
   home.packages = with pkgs; [ socat ];
   programs.eww = {
     enable = true;
-    configDir = ../../res/eww-wayland;
+    configDir = res;
   };
 }
