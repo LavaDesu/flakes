db469ad58f02e43cad303c422fc3bd8f55522730
LavaDesu <me@lava.moe>
2025-03-17 00:41:24 +1100

    user/rofi-wayland: use catppuccin colours

--- modules/user/rofi-wayland.nix
+++ modules/user/rofi-wayland.nix
@@ -1,8 +1,23 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  theme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "rofi-theme";
+    version = "1.0.0";
+    dontUnpack = true;
+    installPhase = ''
+      cp ${../../res/theme.rasi} $out
+      substituteInPlace $out \
+        --replace-fail "CAT_BACKGROUND" "${config.catppuccin.hexcolors.crust}" \
+        --replace-fail "CAT_TEXT" "${config.catppuccin.hexcolors.text}" \
+        --replace-fail "CAT_ACCENT" "${config.catppuccin.hexcolors.${config.catppuccin.accent}}" \
+        --replace-fail "CAT_PLACEHOLDER" "${config.catppuccin.hexcolors.overlay1}"
+    '';
+  };
+in {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
     theme = "theme";
   };
-  xdg.configFile."rofi/theme.rasi".source = ../../res/theme.rasi;
+  xdg.configFile."rofi/theme.rasi".source = theme;
 }
