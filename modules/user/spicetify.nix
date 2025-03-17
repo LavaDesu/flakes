fa2e2c3ce663f34d0afc7894e97f908bdbdf0460
LavaDesu <me@lava.moe>
2025-03-17 13:35:53 +1100

    user/spicetify: switch to catppuccin theme
    i give up with dribbblish

--- modules/user/spicetify.nix
+++ modules/user/spicetify.nix
@@ -1,8 +1,6 @@
 { config, inputs, pkgs, ... }:
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-  colours = builtins.mapAttrs (_: colour: builtins.replaceStrings ["#"] [""] colour) config.catppuccin.hexcolors;
-  accentColour = colours.${config.catppuccin.accent};
 in
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
@@ -10,25 +8,8 @@ in
   programs.spicetify = {
     enable = true;
     alwaysEnableDevTools = true;
-    theme = spicePkgs.themes.dribbblish;
-    customColorScheme = {
-      text               = colours.text;
-      subtext            = colours.subtext1;
-      sidebar-text       = colours.text;
-      main               = colours.base;
-      sidebar            = colours.mantle;
-      player             = colours.base;
-      card               = colours.base;
-      shadow             = colours.mantle;
-      selected-row       = colours.overlay2;
-      button             = colours.overlay1;
-      button-active      = colours.overlay2;
-      button-disabled    = colours.overlay0;
-      tab-active         = colours.surface0;
-      notification       = colours.surface0;
-      notification-error = colours.red;
-      misc               = colours.surface1;
-    };
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = config.catppuccin.flavor;
 
     enabledCustomApps = with spicePkgs.apps; [
       lyricsPlus
