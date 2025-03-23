b89253931d3b16f04151f46f9c0b532a1e57aba7
LavaDesu <me@lava.moe>
2025-03-23 11:12:55 +1100

    user/kitty: add scrollback pager

--- modules/user/kitty.nix
+++ modules/user/kitty.nix
@@ -12,6 +12,8 @@
       color5 = config.catppuccin.hexcolors.mauve;
       color13 = config.catppuccin.hexcolors.mauve;
       window_margin_width = 5;
+      scrollback_pager = ''nvim --noplugin -c "set signcolumn=no showtabline=0" -c "silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - " -c "autocmd VimEnter * normal G"'';
+      scrollback_pager_history_size = 2;
     };
   };
 }
