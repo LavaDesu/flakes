5f5412f433f2c121b3888400d283fb70effe32f0
LavaDesu <me@lava.moe>
2025-04-05 14:11:56 +1100

    services/nginx: remove hosts

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -21,28 +21,5 @@
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
-
-    virtualHosts = {
-      "lava.moe" = {
-        useACMEHost = "lava.moe";
-        forceSSL = true;
-        root = inputs.website.outPath;
-      };
-      "cdn.lava.moe" = {
-        useACMEHost = "lava.moe";
-        forceSSL = true;
-        root = "/persist/cdn";
-      };
-      "_" = {
-        default = true;
-        addSSL = true;
-        # TODO generate this somewhere
-        sslCertificate = "/persist/fakeCerts/fake.crt";
-        sslCertificateKey = "/persist/fakeCerts/fake.key";
-        extraConfig = ''
-          return 444;
-        '';
-      };
-    };
   };
 }
