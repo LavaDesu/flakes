e7a7b39d411d4bcf95b92f2dd0326466c39de3a8
LavaDesu <me@lava.moe>
2025-04-05 14:33:57 +1100

    hosts/hazel: init immich

--- hosts/hazel/default.nix
+++ hosts/hazel/default.nix
@@ -42,4 +42,27 @@
     forceSSL = true;
     enableACME = true;
   };
+
+  services.immich = {
+    enable = true;
+    port = 2283;
+  };
+
+  users.users.immich.extraGroups = [ "video" "render" ];
+  hardware.opengl.enable = true;
+  services.nginx.virtualHosts."photos.lava.moe" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://[::1]:${toString config.services.immich.port}";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        client_max_body_size 50000M;
+        proxy_read_timeout   600s;
+        proxy_send_timeout   600s;
+        send_timeout         600s;
+      '';
+    };
+  };
 }
