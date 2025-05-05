cc2adb3aa803350b9fb6ae9f041f19b96a2feee3
LavaDesu <me@lava.moe>
2025-05-05 17:26:44 +1000

    services/syncthing: use tmpfile rules to create data directories

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,4 +1,14 @@
-{ ... }: {
+{ config, ... }:
+let
+  dir = "/persist/shared/.syncthing";
+  uid = toString config.users.users.rin.uid;
+  gid = toString config.users.groups.users.gid;
+in
+{
+  systemd.tmpfiles.rules = [
+    "d ${dir}/config 700 ${uid} ${gid}"
+    "d ${dir}/data 700 ${uid} ${gid}"
+  ];
   systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true";
   services.syncthing = {
     enable = true;
@@ -7,16 +17,5 @@
     group = "users";
     dataDir = "/persist/shared/.syncthing/data";
     configDir = "/persist/shared/.syncthing/config";
-    settings = {
-      devices = {
-        #"anemone".id = "";
-      };
-      # folders = {
-      #   "Obby" = {
-      #     path = "/home/rin/Documents/Obby/Obby";
-      #     devices = [];
-      #   };
-      # };
-    };
   };
 }
