945eb34e6042778692f885954d8058dc6da6ac8b
LavaDesu <me@lava.moe>
2025-05-19 14:14:42 +1000

    hazel/networking: add ipv6 address

--- hosts/hazel/networking.nix
+++ hosts/hazel/networking.nix
@@ -1,5 +1,15 @@
 { config, ... }: {
   networking = {
     useDHCP = true;
+    interfaces.enp8s0.ipv6.addresses = [
+      {
+        address = "2a01:4f9:4a:2694::11";
+        prefixLength = 64;
+      }
+    ];
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp8s0";
+    };
   };
 }
