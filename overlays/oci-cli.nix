a4b2db236a4dd7481656e7b707e7ed84f37834dd
LavaDesu <me@lava.moe>
2025-05-20 01:18:42 +1000

    patch

--- overlays/oci-cli.nix
+++ overlays/oci-cli.nix
@@ -1,5 +1,5 @@
 self: super: {
   oci-cli = super.oci-cli.overrideAttrs(o: {
-    patches = o.patches ++ [ ./patches/oci.patch ];
+    patches = (o.patches or []) ++ [ ./patches/oci.patch ];
   });
 }
