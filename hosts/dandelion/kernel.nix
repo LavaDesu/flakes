4bc19ed87e8420b0eb2f868093b60ab8d55da7d0
LavaDesu <me@lava.moe>
2025-05-19 20:24:01 +1000

    dandelion/kernel: remove duplicate sysctl

--- hosts/dandelion/kernel.nix
+++ hosts/dandelion/kernel.nix
@@ -6,9 +6,5 @@
     };
     initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
     initrd.kernelModules = [ "nvme" ];
-    kernel.sysctl = {
-      "kernel.core_pattern" = "|/bin/false";
-      "kernel.sysrq" = 1;
-    };
   };
 }
