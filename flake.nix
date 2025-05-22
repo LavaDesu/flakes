fe8d26a860d16ad80477436b42110507101caf16
LavaDesu <me@lava.moe>
2025-05-23 01:31:42 +1000

    workflows/cachix: some more advanced waiting
    i hope this works i have no idea why git-crypt does this it also happens
    on local computers sometimes jalsdfjasoigrjoi

--- flake.nix
+++ flake.nix
@@ -78,6 +78,8 @@
       nixosConfigurations."hazel" = mkSystem nixpkgs-stable "hazel" "x86_64-linux" [];
       nixosConfigurations."hyacinth" = mkSystem nixpkgs "hyacinth" "x86_64-linux" [];
 
+      checks."x86_64-linux".is_dirty = if !(self ? rev) then throw "Dirty git tree detected." else self.rev;
+
       packages."x86_64-linux" =
         let
           pkgs = import nixpkgs rec {
