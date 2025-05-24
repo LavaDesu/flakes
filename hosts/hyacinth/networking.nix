0f1271badb39214b534e15de936cc4d6b318bab8
LavaDesu <me@lava.moe>
2025-05-24 20:22:32 +1000

    hyacinth/networking: update ip address

--- hosts/hyacinth/networking.nix
+++ hosts/hyacinth/networking.nix
@@ -5,10 +5,10 @@
     interfaces.enp5s0.useDHCP = false;
 
     interfaces.enp5s0.ipv4.addresses = [{
-      address = "192.168.0.151";
+      address = "192.168.1.201";
       prefixLength = 24;
     }];
-    defaultGateway = "192.168.0.1";
+    defaultGateway = "192.168.1.1";
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
 
     extraHosts = ''
